3522be8542a7f6572ef831b499af637b01f8f4b7
NicoMore <nicomorelli47@gmail.com>
2021-04-21 16:36:13 -0300

    Cambios

--- Prenda.java
+++ Prenda.java
@@ -1,11 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+enum Categoria { SUPERIOR, INFERIOR, CALZADO, ACCESORIO; }
+
 abstract class Prenda {
-    TipoPrenda tipo;
+    String tipo;
     String material;
     String color;
     String colorSecundario;
 
-    public static Prenda cargarPrenda() {
-        Prenda prenda = this.nuevaPrenda();
+    public Prenda cargarPrenda() {
+        Prenda prenda = nuevaPrenda();
         prenda.indicarTipo();
         prenda.elegirMaterial();
         prenda.elegirColor();
@@ -14,28 +19,105 @@ abstract class Prenda {
         return prenda;
     }
 
+    public static int seleccionarCategoria() {
+        System.out.println("Seleccione categoria:");
+        System.out.print("1. Prenda superior");
+        System.out.print("2. Prenda inferior");
+        System.out.print("3. Calzado");
+        System.out.print("4. Accesorio");
+
+        try { 
+            Scanner scan = new Scanner(System.in);
+
+            int eleccion = scan.nextInt();
+            scan.close();
+            return eleccion;
+        }
+        catch (IllegalArgumentException tipoErroneo) {
+            System.out.println("Se introdujo un valor invalido.");
+            return 0;
+        }      
+    }
+
+    void elegirMaterial() {
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Indique material:");
+
+            this.material = scan.nextLine();
+            scan.close();
+        }
+        catch (IllegalArgumentException tipoErroneo) {
+            System.out.println("Se introdujo un valor de material invalido.");
+        }
+    }
+
+    void elegirColor() {
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Indique color:");
+
+            this.color = scan.nextLine();
+            scan.close();
+        }
+        catch (IllegalArgumentException tipoErroneo) {
+            System.out.println("Se introdujo un valor de color invalido.");
+        } 
+    }
+
+    void elegirColorSecundario() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Desea agregar un color secundario?");
+
+        boolean deseaColorSecundario = scan.nextBoolean();
+        if (deseaColorSecundario) {
+            try {
+                System.out.println("Indique color secundario:");
+
+                this.colorSecundario = scan.nextLine();
+                scan.close();
+            }
+            catch (IllegalArgumentException tipoErroneo) {
+                System.out.println("Se introdujo un valor de color invalido.");
+            }
+        }
+    }
+
+    void indicarTipo() {
+        // Solicitaria que usuario seleccione de la lista tiposCompatibles (de las subclases) un tipo.
+    }
+
     abstract Prenda nuevaPrenda();
+
 }
 
 class PrendaSuperior extends Prenda {
+    ArrayList<String> tiposCompatibles = new ArrayList<>();
+
     Prenda nuevaPrenda() {
         return new PrendaSuperior();
     }
 }
 
 class PrendaInferior extends Prenda {
+    ArrayList<String> tiposCompatibles = new ArrayList<>();
+
     Prenda nuevaPrenda() {
         return new PrendaInferior();
     }
 }
 
 class Calzado extends Prenda {
+    ArrayList<String> tiposCompatibles = new ArrayList<>();
+
     Prenda nuevaPrenda() {
         return new Calzado();
     }
 }
 
 class Accesorio extends Prenda {
+    ArrayList<String> tiposCompatibles = new ArrayList<>();
+
     Prenda nuevaPrenda() {
         return new Accesorio();
     }
--- QMP.java
+++ QMP.java
@@ -1,8 +1,29 @@
 public class QueMePongo {
-    public static void main(Sring[] args) {
-        while(true) {
-            Prenda.cargarPrenda();
+    public static void main(String[] args) {
+        while(true) {           
+            switch (Prenda.seleccionarCategoria() - 1) {
+                case -1:
+                    System.out.println("Hubo un error al elegir categoria, intente nuevamente.");
+                    break;
+                case SUPERIOR:
+                    Prenda nuevaPrendaSuperior = new PrendaSuperior().cargarPrenda();
+                    break;
+                case INFERIOR:
+                    Prenda nuevaPrendaInferior = new PrendaInferior().cargarPrenda();
+                    break;
+                case CALZADO:
+                    Prenda nuevoCalzado = new Calzado().cargarPrenda();
+                    break;
+                case ACCESORIO:
+                    Prenda nuevoAccesorio = new Accesorio().cargarPrenda();
+                    break;
+            }
+
+
+            //Condicion de corte
         }
+
+        //Posible generacion de atuendos
     }
 
 
